f033e9fb9be8bb4fe61a0a7b18ea36944b110e9c
Genichi Maruo <gen1010f8e7@gmail.com>
2025-05-09 16:32:16 +0900

    feat: update build script and package dependencies for improved functionality

--- build_apps.sh
+++ build_apps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# client_app と server_app のクリーンアップ
+make -C client clean
+make -C server clean
+echo "✅ client_app and server_app cleaned."
+
 # client_app のビルド
 make -C client
 echo "✅ client_app compiled."
